b98cf836e5d76a5f9d7af235ad3ed4a261df10d6
Eva Smidt <shevangeline@gmail.com>
2018-01-28 21:32:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-if board == " " || board == ""
+   board == " " || board == ""
 return false
-else board == "X" || board == "O"
+board == "X" || board == "O"
 return true
 end
