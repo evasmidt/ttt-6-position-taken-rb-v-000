462f253fb752a2d017e53e8d5a3a0fd191fd5feb
Eva Smidt <shevangeline@gmail.com>
2018-01-28 21:41:03 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -20,6 +20,8 @@
 def position_taken? (board, index)
 if board[index] == " " || board[index] == ""
   return false
+elsif board[index] == nil
+  return false
 else board[index] == "X" || board[index] == "O"
   return true
 end
