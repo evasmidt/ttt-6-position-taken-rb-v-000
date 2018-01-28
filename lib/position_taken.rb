1acf00f837b63f3b2c40a8b10c12c71c64ab6386
Eva Smidt <shevangeline@gmail.com>
2018-01-28 21:36:56 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,15 @@
 # code your #position_taken? method here!
-def position_taken? (board, index)
-   board == " " || board == ""
-return false
-end
-def position_taken? (board, index)
-board == "X" || board == "O"
-return true
+#def position_taken? (board, index)
+   #board == " " || board == ""
+#return false
+#end
+#def position_taken? (board, index)
+#board == "X" || board == "O"
+#return true
+#end
+
+
+if board == " " || board == ""
+else board == "X" || board == "O"
+  return true
 end
